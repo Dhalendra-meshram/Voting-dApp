14faa082fea4ac554242aab026b46508af7253da
Dhalendra Meshram <104065445+Dhalendra-meshram@users.noreply.github.com>
2026-07-29 19:32:12 +0000

    Show MongoDB connection error

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,9 +4,9 @@ import { ENV } from "./env.js";
 export const connectDB = async () => {
   try {
     await mongoose.connect(ENV.MONGO_URI as string);
-    console.log("MongoDB Connected ");
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection failed ");
+    console.error("❌ MongoDB connection failed:", error);
     process.exit(1);
   }
 };
